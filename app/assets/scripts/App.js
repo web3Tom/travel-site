702bd74243018d4d79cc02c84dc12da468537651
TJ <web3hrive@protonmail.com>
2022-03-27 16:52:59 -0400

    Completed Mobile Menu animation.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-  module.hot.accpet();
+  module.hot.accept();
 }
