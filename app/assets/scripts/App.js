d86b71938313c079be0b7fc61fda2d78fc68e796
TJ <web3hrive@protonmail.com>
2022-03-28 18:18:15 -0400

    Completed the reveal-on-scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+// let revealOnScroll = new RevealOnScroll(); -- remove var for recyclability of module
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 65);
 
 let mobileMenu = new MobileMenu();
 
