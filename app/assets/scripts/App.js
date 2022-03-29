51b5d711a6de50af071116466146c9e2dcbea2f2
TJ <web3hrive@protonmail.com>
2022-03-28 21:49:33 -0400

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 // let revealOnScroll = new RevealOnScroll(); -- remove var for recyclability of module
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 65);
+let stickyHeader = new StickyHeader();
 
 let mobileMenu = new MobileMenu();
 
